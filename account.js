fc82e57637fff46f39231ba7a6117b6670589895
矿井小帕的血小板 <61276297+MineCreeper86@users.noreply.github.com>
2022-07-21 18:08:17 +0800

    Update account.js

--- account.js
+++ account.js
@@ -28,6 +28,7 @@ function reg(){
 	}
 	};
 	xhttp.open("POST", "https://api.jcsuf.top/api/register", true);
+	xhttp.withCredentials = true;
 	xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded")//必须要设置，被坑了一天啊
 	xhttp.send("name="+utf8Str+"&password="+document.getElementById("i2").value+"&mail="+document.getElementById("i3").value);
 }
