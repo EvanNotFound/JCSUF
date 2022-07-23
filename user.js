b8e908598cd0ba64b64b73a554efaaeb629b0772
矿井小帕的血小板 <61276297+MineCreeper86@users.noreply.github.com>
2022-07-23 10:14:36 +0800

    BUG FIX AND ALERT RESULT

--- user.js
+++ user.js
@@ -1,5 +1,7 @@
+var newname = "";
+
 function rename() {
-    var newname = prompt("请输入您的新用户名","不应为空或相似于JCSUF Admin");
+    newname = prompt("请输入您的新用户名","不应为空或相似于JCSUF Admin");
 	let utf8Str=''
     for (let i=0; i<newname.length;i++){
         let t = newname[i]
@@ -14,17 +16,24 @@ function rename() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
-	    	document.getElementById("namefield").innerHTML = "<b><big>"+JSON.parse(this.responseText).name+"</big></b>";
-		    document.getElementById("more-namefield").innerHTML = "<b>"+JSON.parse(this.responseText).name+"</b>";
-		    document.getElementById("ava-img").src = JSON.parse(this.responseText).avatar;
-	    	currentid = JSON.parse(this.responseText).uid;
-	    	admin = JSON.parse(this.responseText).admin_level;
-	    	process_level(JSON.parse(this.responseText).exp);
-	    	if(JSON.parse(this.responseText).uid!=-1){
-		    	document.getElementById("signin-compose").title = "发帖";
-			    document.getElementById("signin-compose").innerHTML = "发帖";
-	    		document.getElementById("signin-compose").href = "compose.html";
-		    }
+	    	switch(JSON.parse(this.responseText).code){
+                case 0:
+                alert("改名成功");
+                document.getElementById("namefield").innerHTML = "<b><big>"+newname+"</big></b>";
+		        document.getElementById("more-namefield").innerHTML = "<b>"+newname+"</b>";
+				break;
+				case 1:
+				alert("你不登陆改个什么？");
+				break;
+				case 2:
+				alert("您已被封禁");
+				break;
+                case 3:
+				alert("不合法的用户名");
+				break;
+				case 999:
+				alert("管理员正在修正服务器数据，请稍等几秒并重试");
+            }
 	    }
     };
     xhttp.open("GET", "https://api.jcsuf.top/api/rename?newname="+utf8Str, true);
