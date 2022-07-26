f58dd8045e3a409c3f5cb356e1aae4cfee670a49
矿井小帕的血小板 <61276297+MineCreeper86@users.noreply.github.com>
2022-07-26 13:09:05 +0800

    login user only

--- user.js
+++ user.js
@@ -17,23 +17,23 @@ function rename() {
     xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
 	    	switch(JSON.parse(this.responseText).code){
-                case 0:
-                alert("改名成功");
-                document.getElementById("namefield").innerHTML = "<b><big>"+newname+"</big></b>";
-		        document.getElementById("more-namefield").innerHTML = "<b>"+newname+"</b>";
+        case 0:
+        alert("改名成功");
+        document.getElementById("namefield").innerHTML = "<b><big>"+newname+"</big></b>";
+		    document.getElementById("more-namefield").innerHTML = "<b>"+newname+"</b>";
 				break;
 				case 1:
-				alert("你不登陆改个什么？");
+				alert("你不登录改个什么？");
 				break;
 				case 2:
 				alert("您已被封禁");
 				break;
-                case 3:
+        case 3:
 				alert("不合法的用户名");
 				break;
 				case 999:
 				alert("管理员正在修正服务器数据，请稍等几秒并重试");
-            }
+        }
 	    }
     };
     xhttp.open("GET", "https://api.jcsuf.top/api/rename?newname="+utf8Str, true);
