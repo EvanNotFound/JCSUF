470b43a9e012cb6e4c98b5d2d6a827eb866c2b6a
矿井小帕的血小板 <61276297+MineCreeper86@users.noreply.github.com>
2022-07-29 22:50:28 +0800

    kyou made 10%

--- post.js
+++ post.js
@@ -3,7 +3,7 @@ xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var rbody = JSON.parse(this.responseText)
 		for(let i = 0; i < rbody.length; i++){
-			if(rbody[i].id>=1000) document.getElementById("right-container").innerHTML += '<div class="subforum-sort" id="parent-banner-'+rbody[i].id+'"><h1>'+rbody[i].name+'demo</h1></div>';
+			if(rbody[i].id>=1000) document.getElementsByClassName("right-container")[0].innerHTML += '<div class="subforum-sort" id="parent-banner-'+rbody[i].id+'"><h1>'+rbody[i].name+'demo</h1></div>';
 		}
 	}
 };
