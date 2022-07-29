338453808044883a041d5321086d74d07e167539
矿井小帕的血小板 <61276297+MineCreeper86@users.noreply.github.com>
2022-07-29 22:48:18 +0800

    wofulea

---
+++ post.js
@@ -0,0 +1,12 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		var rbody = JSON.parse(this.responseText)
+		for(let i = 0; i < rbody.length; i++){
+			if(rbody[i].id>=1000) document.getElementById("right-container").innerHTML += '<div class="subforum-sort" id="parent-banner-'+rbody[i].id+'"><h1>'+rbody[i].name+'demo</h1></div>';
+		}
+	}
+};
+xhttp.open("GET", "https://api.jcsuf.top/api/categorylist", true);
+xhttp.withCredentials = true;
+xhttp.send();
