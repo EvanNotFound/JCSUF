73f0bca816e1a84a90ed49060f0d2920ab6ce69e
Baka Cirno 999 <14835957@qq.com>
2022-08-26 12:59:43 +0800

    ahhhhaaa

--- bilibind.js
+++ bilibind.js
@@ -19,13 +19,15 @@ xhttp.withCredentials = true;
 xhttp.send();
 
 function refresh(){
+    if(!stop){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 	    switch(JSON.parse(this.responseText).code){
 	    	case 0:
-	    	if(JSON.parse(this.responseText).received&&!stop){
+	    	if(JSON.parse(this.responseText).received){
                 document.getElementById("verify-status").innerHTML = '<i class="fa-solid fa-circle-check"></i><span style="color:#333333; font-size:18px">&nbsp;已验证</span>'
+                stop = true;
             }
 	    	break;
 	    }
@@ -34,4 +36,5 @@ function refresh(){
     xhttp.open("GET", "https://api.jcsuf.top/api/bilibind", true);
     xhttp.withCredentials = true;
     xhttp.send();
+    }
 }
