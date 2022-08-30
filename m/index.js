9040d67dcadbc18cb4c371b8961106e11de77207
Baka Cirno 999 <14835957@qq.com>
2022-08-30 11:31:57 +0800

    问题不大个der

--- m/index.js
+++ m/index.js
@@ -1,4 +1,4 @@
-new Vue({
+var vm = new Vue({
     el: '#articles',
     data: {
         articles: [
@@ -11,16 +11,20 @@ new Vue({
         ]
     },
     methods:{
-        getArticle(){
+        getArticles:function(){
             //发送get请求
             console.log("www")
             this.$http.get('https://api.jcsuf.top/api/fetchnewarticle?pc=50').then(function(res){
                 console.log(res.body);
                 console.log(res.body.articles);
-                return res.body.articles;
+                vm.articles = res.body.articles;
             },function(){
                 console.log('请求失败处理');
             });
+            return this.articles;
         },
     },
+    mounted(){
+        this.getArticles()
+    }
 })
