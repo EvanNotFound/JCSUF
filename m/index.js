f712747d5e2ab3940beef7787af5fb95e05d9933
Baka Cirno 999 <14835957@qq.com>
2022-08-30 12:53:50 +0800

    mobile index

--- m/index.js
+++ m/index.js
@@ -1,17 +1,11 @@
 var vm = new Vue({
     el: '#articles',
     data: {
-        articles: [
-            {
-                title: "Feel good",
-                snapshot: "突然感觉这东西也没这么难写",
-                read: 114,
-                ccount: 514
-            }
-        ]
+        articles: [],
+        ababa: 0
     },
     methods:{
-        getArticles:function(){
+        getArticles:function() {
             //发送get请求
             console.log("www")
             this.$http.get('https://api.jcsuf.top/api/fetchnewarticle?pc=50').then(function(res){
