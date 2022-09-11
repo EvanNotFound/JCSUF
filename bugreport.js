c3e54728ede6a448697d45ad289f9df7d43a8711
Baka Cirno 999 <14835957@qq.com>
2022-09-11 09:49:10 +0800

    bugreport

---
+++ bugreport.js
@@ -0,0 +1,9 @@
+function report() {
+	document.getElementById("bgmask").style.display = "block";
+	document.getElementById("report-container").style.display = "block";
+}
+
+function minimize_pane() {
+	document.getElementById("bgmask").style.display = "none";
+	document.getElementById("report-container").style.display = "none";
+}
