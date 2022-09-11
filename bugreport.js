748244717ff2b3e3c5a8acd5b9a32b50d2c872a4
Baka Cirno 999 <14835957@qq.com>
2022-09-11 21:38:26 +0800

    aa

--- bugreport.js
+++ bugreport.js
@@ -7,3 +7,13 @@ function minimize_pane() {
 	document.getElementById("bgmask").style.display = "none";
 	document.getElementById("report-container").style.display = "none";
 }
+
+function post_report() {
+    if(!document.getElementById("i1").checked){
+		if(prompt("不勾选采集浏览器数据会降低上报成功率!\n确定不勾选请输入“继续操作”")=="继续操作") post_report_checked();
+	}
+}
+
+function post_report_checked(){
+
+}
