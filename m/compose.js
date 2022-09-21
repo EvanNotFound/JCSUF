f09e039b67f3bf79cdbdc761911147ceba6dbf7c
Baka Cirno 999 <14835957@qq.com>
2022-09-21 21:38:21 +0800

    aaa

--- m/compose.js
+++ m/compose.js
@@ -1,3 +1,11 @@
+var lastVar = "";
+setInterval(function(){
+	if(lastVar!=document.getElementById("i2").value.replaceAll('\n','<br>')){
+		lastVar = document.getElementById("i2").value.replaceAll('\n','<br>')
+		document.getElementById("preview").innerHTML = lastVar
+	}
+},100)
+
 var vm = new Vue({
     el: '.vue-enable',
     data: {
