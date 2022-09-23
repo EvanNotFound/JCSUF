b139de45a288b65856113e92aa904ebd0ff722f0
Baka Cirno 999 <14835957@qq.com>
2022-09-23 14:43:19 +0800

    cc

--- m/compose.js
+++ m/compose.js
@@ -6,6 +6,37 @@ setInterval(function(){
 	}
 },100)
 
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		var rbody = JSON.parse(this.responseText)
+		for(let i = 0; i < rbody.length; i++){
+			if(rbody[i].id>=1000) document.getElementById("maincate").innerHTML += '<option value="'+rbody[i].id+'">'+rbody[i].name+'</option>';
+		}
+	}
+};
+xhttp.open("GET", "https://api.jcsuf.top/api/categorylist", true);
+xhttp.withCredentials = true;
+xhttp.send();
+
+function loadbranch(val){
+	if(val!=-1){
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var rbody = JSON.parse(this.responseText)
+				document.getElementById("i3").innerHTML = '<option value="-1">--未选择--</option>';
+				for(let i = 0; i < rbody.length; i++){
+					document.getElementById("i3").innerHTML += '<option value="'+rbody[i].id+'">'+rbody[i].name+'</option>';
+				}
+			}
+		};
+		xhttp.open("GET", "https://api.jcsuf.top/api/categorylist?parent="+val, true);
+		xhttp.withCredentials = true;
+		xhttp.send();
+	}
+}
+
 var vm = new Vue({
     el: '.vue-enable',
     data: {
