ca722b947d5b7340992ee190a8b3eaa5fc644370
Baka Cirno 999 <14835957@qq.com>
2022-09-25 17:03:36 +0800

    mobile~~~~~~~~

--- m/compose.js
+++ m/compose.js
@@ -1,4 +1,8 @@
 var lastVar = "";
+var votetitle = undefined;
+var voteend = -1;
+var votetype = 0;
+var voteanon = false;
 setInterval(function(){
 	if(lastVar!=document.getElementById("i2").value.replaceAll('\n','<br>')){
 		lastVar = document.getElementById("i2").value.replaceAll('\n','<br>')
@@ -42,13 +46,90 @@ function display_user_option(checked){
 	else document.getElementById("more-option").style.display = "none";
 }
 
+function post(){
+	var xhttp2 = new XMLHttpRequest();
+	var str = document.getElementById("i2").value;
+	let utf8Str=''
+    for (let i=0; i<str.length;i++){
+        let t = str[i]
+        let text = ''
+        if(t.charCodeAt(0)>=4096){
+          text = "uN1c0dE"+t.charCodeAt(0).toString(16).toLowerCase();
+        }else if(t.charCodeAt(0)>=256){
+			text = "uN1c0dE0"+t.charCodeAt(0).toString(16).toLowerCase();
+		}else{
+          text = encodeURIComponent(t)
+        }
+        utf8Str += text
+    }
+	var str2 = document.getElementById("i1").value;
+	let utf8Str2=''
+    for (let i=0; i<str2.length;i++){
+        let t = str2[i]
+        let text = ''
+        if(t.charCodeAt(0)>=256){
+          text = "uN1c0dE"+t.charCodeAt(0).toString(16).toLowerCase();
+        }else{
+          text = encodeURIComponent(t)
+        }
+        utf8Str2 += text
+     }
+	xhttp2.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			switch(JSON.parse(this.responseText).code){
+				case 0:
+				location.href = "https://www.jcsuf.top/sendSuccess.html"
+				break;
+				case 1:
+				alert("发布失败，请检查登录状态等内容");
+				break;
+				case 2:
+				alert("您已被封禁");
+				break;
+				case 3:
+				alert("未选择分区");
+				break;
+				case 4:
+				alert("你什么身份，我什么地位？");
+				break;
+				case 999:
+				alert("管理员正在修正服务器数据，请稍等几秒并重试");
+			}
+		}
+	};
+	xhttp2.open("POST", "https://api.jcsuf.top/api/postarticle", true);
+	xhttp2.withCredentials = true;
+	xhttp2.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+	var reqparam = "category="+document.getElementById("i3").value+"&anonymous="+document.getElementById("i4").checked+"&content="+utf8Str.replaceAll("\n","<br>")+"&title="+utf8Str2.replaceAll("\n","<br>")+"&tag=0&nsfw="+document.getElementById("i5").checked+"&lonly="+document.getElementById("i6").checked+"&political="+document.getElementById("i7").checked+"&unsearch="+document.getElementById("i8").checked+"&restrictreg="+document.getElementById("i9").checked+"&levellimit="+document.getElementById("i10").value;
+	if(votetitle!=undefined){
+		if(votetype==0){
+			reqparam += "&vote=";
+			for (let i=0; i<votetitle.length;i++){
+				let t = votetitle[i]
+				let text = ''
+				if(t.charCodeAt(0)>=4096){
+				  text = "uN1c0dE"+t.charCodeAt(0).toString(16).toLowerCase();
+				}else if(t.charCodeAt(0)>=256){
+					text = "uN1c0dE0"+t.charCodeAt(0).toString(16).toLowerCase();
+				} else {
+				  text = encodeURIComponent(t)
+				}
+				reqparam += text
+			 }
+			reqparam += "&votetype=0";
+			reqparam += "&voteend="+voteend;
+			reqparam += "&voteanonymous="+voteanon;
+		}
+	}
+	xhttp2.send(reqparam);
+}
+
 var vm = new Vue({
     el: '.vue-enable',
     data: {
-
+		but_visible: false
     },
     methods: {
-        
     },
     mounted() {
         
