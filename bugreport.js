8c29441d501aa20adbd06c7f693b0244339ba95c
Baka Cirno 999 <14835957@qq.com>
2022-09-25 21:56:28 +0800

    simple

--- bugreport.js
+++ bugreport.js
@@ -65,4 +65,6 @@ xhgw.send();
 
 function processOrder(order) {
 	document.getElementById("main-content").innerHTML += '<div class="order-body" highlight="no" id="order-'+order.oid+'"></div>'
+	document.getElementById("floor-"+order.oid).innerHTML += '<div class="userinfo" id="author-comment'+order.oid+'"><img src="https://upload.thwiki.cc/thumb/0/0a/%E9%AC%BC%E4%BA%BA%E6%AD%A3%E9%82%AA%EF%BC%88Q%E7%89%88%E7%AB%8B%E7%BB%98%EF%BC%89.png/100px-%E9%AC%BC%E4%BA%BA%E6%AD%A3%E9%82%AA%EF%BC%88Q%E7%89%88%E7%AB%8B%E7%BB%98%EF%BC%89.png" height="64px" class="left-avatar" id="author-avatar-'+order.oid+'" style="margin: 0 auto;"><br><b><span id="author-name-'+order.oid+'">作者</span></b><br><span id="author-level-'+order.oid+'">LvXX</span></div>'
+	document.getElementById("floor-"+comment.floor).innerHTML += '<div class="content-container"><div class="content" id="content-'+comment.floor+'">'+order.content+'</div></div>'
 }
