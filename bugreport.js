81d3723622d16eb7f581774663a6823ce282880f
Baka Cirno 999 <14835957@qq.com>
2022-09-27 21:03:52 +0800

    以后慢慢弄

--- bugreport.js
+++ bugreport.js
@@ -67,5 +67,9 @@ xhgw.send();
 function processOrder(order) {
 	document.getElementById("main-content").innerHTML += '<div class="order-body" highlight="no" id="order-'+order.oid+'"></div>'
 	document.getElementById("order-"+order.oid).innerHTML += '<div class="userinfo" id="author-comment'+order.oid+'"><img src="https://upload.thwiki.cc/thumb/0/0a/%E9%AC%BC%E4%BA%BA%E6%AD%A3%E9%82%AA%EF%BC%88Q%E7%89%88%E7%AB%8B%E7%BB%98%EF%BC%89.png/100px-%E9%AC%BC%E4%BA%BA%E6%AD%A3%E9%82%AA%EF%BC%88Q%E7%89%88%E7%AB%8B%E7%BB%98%EF%BC%89.png" height="64px" class="left-avatar" id="author-avatar-'+order.oid+'" style="margin: 0 auto;"><br><b><span id="author-name-'+order.oid+'">作者</span></b><br><span id="author-level-'+order.oid+'">LvXX</span></div>'
-	document.getElementById("order-"+order.oid).innerHTML += '<div class="content-container"><div class="content" id="content-'+order.oid+'"><font style="color:rgb(68,68,100);font-size:10px">Work Order ID '+order.oid+'</font><br>'+order.content+'</div></div>'
+	document.getElementById("order-"+order.oid).innerHTML += '<div class="content-container"><div class="content" id="content-'+order.oid+'"><font style="color:rgb(68,68,100);font-size:10px">Work Order ID '+order.oid+'</font><br>'+order.content+'</div><div class="subreply" id="order-trace-'+order.oid+'"></div></div>'
+	for(const tr of order.trace_flow){
+		document.getElementById("order-trace-"+order.oid).innerHTML += '<span><font color="gray">'+comment.html+'</font></span>'
+		document.getElementById("order-trace-"+order.oid).style.backgroundColor = "white"
+	}
 }
